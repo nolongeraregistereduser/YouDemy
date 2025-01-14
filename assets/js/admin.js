295ecd8459458022c5cc8ad4b6cb809aeec649b1
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-01-14 17:01:24 +0100

    Burger Menue to FrontEnd

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const sidebar = document.querySelector('.sidebar');
-    const mainContent = document.querySelector('.main-content');
 
-    menuToggle.addEventListener('click', function() {
-        sidebar.classList.toggle('active');
-    });
+    if (menuToggle && sidebar) {
+        menuToggle.addEventListener('click', function() {
+            sidebar.classList.toggle('active');
+        });
 
-    // Close sidebar when clicking outside
-    mainContent.addEventListener('click', function() {
-        if(sidebar.classList.contains('active')) {
-            sidebar.classList.remove('active');
-        }
-    });
+        // Fermer le menu si on clique en dehors
+        document.addEventListener('click', function(e) {
+            if (!sidebar.contains(e.target) && !menuToggle.contains(e.target)) {
+                sidebar.classList.remove('active');
+            }
+        });
+    }
 }); 
