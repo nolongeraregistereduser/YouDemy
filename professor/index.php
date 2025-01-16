757886674a6035aeef873d570d83cd11a1d76af7
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-01-16 10:51:31 +0100

    Adding course for professor

--- professor/index.php
+++ professor/index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    
+    <link rel="stylesheet" href="includes/styleProf.css">
 
 </head>
 <body>
