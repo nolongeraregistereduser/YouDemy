ef7f2370600816e3da3b0a8506552da258d592f0
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-01-17 09:45:15 +0100

    Style Path

--- Student/includes/footer.php
+++ Student/includes/footer.php
@@ -4,7 +4,7 @@
    Footer Example
   </title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
-  <link rel="stylesheet" href="includes/styleStudent.css">
+  <link rel="stylesheet" href="styleStudent.css">
  </head>
  <body>
   <div class="footer">
--- Student/includes/navbar.php
+++ Student/includes/navbar.php
@@ -3,7 +3,7 @@
     <title>YouDemy Header</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="includes/styleStudent.css">
+    <link rel="stylesheet" href="styleStudent.css">
 </head>
 <body>
     <div class="top-banner">
